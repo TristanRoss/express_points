36a88ac8449e0ccb8220a6b7620e4b2108e521f0
TristanRoss <tgr1998@live.com>
2021-03-23 20:31:48 -0500

    Complete project

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/api/payers', require('./routes/api/payers'));
--- routes/api/payers.js
+++ routes/api/payers.js
@@ -1,14 +1,113 @@
 const express = require('express');
 const router = express.Router();
 
-const balances = [
-  { payer: 'DANNON', points: -100 },
-  { payer: 'UNILEVER', points: -200 },
-  { payer: 'MILLER COORS', points: -4700 },
+let balances = {};
+
+const transactions = [
+  { payer: 'DANNON', points: 1000, timestamp: '2020-11-02T14:00:00Z' },
+  { payer: 'UNILEVER', points: 200, timestamp: '2020-10-31T11:00:00Z' },
+  { payer: 'DANNON', points: -200, timestamp: '2020-10-31T15:00:00Z' },
+  { payer: 'MILLER COORS', points: 10000, timestamp: '2020-11-01T14:00:00Z' },
+  { payer: 'DANNON', points: 300, timestamp: '2020-10-31T10:00:00Z' },
 ];
 
-const transactions = [];
+router.get('/', (req, res) => res.send(balances));
 
-router.get('/', (req, res) => res.json(balances));
+router.post('/add', (req, res) => {
+  const newTransaction = {
+    ...req.body,
+  };
+
+  if (
+    !newTransaction.payer ||
+    !newTransaction.points ||
+    !newTransaction.timestamp
+  ) {
+    return res.status(400).json({
+      msg: 'Please include a payer, points, and a timestamp',
+    });
+  }
+
+  transactions.push(newTransaction);
+  res.json({ msg: 'Transaction added', transaction: newTransaction });
+});
+
+router.post('/spend', (req, res) => {
+  const pointsReq = {
+    ...req.body,
+  };
+
+  if (!pointsReq.points) {
+    return res.status(400).json({ msg: 'Please include points' });
+  }
+
+  let points = pointsReq.points;
+  const transactionsCopy = transactions.slice(0);
+  transactionsCopy.sort(
+    (a, b) => new Date(b.timestamp) - new Date(a.timestamp)
+  );
+
+  const spent = [];
+  while (transactionsCopy.length > 0) {
+    const current = transactionsCopy.pop();
+
+    if (balances[current.payer] == null) {
+      if (current.points >= 0) {
+        balances[current.payer] = current.points;
+        if (points > current.points) {
+          points -= current.points;
+          let s = {};
+          s['payer'] = current.payer;
+          s['points'] = -current.points;
+          spent.push(s);
+          balances[current.payer] = 0;
+        } else {
+          balances[current.payer] -= points;
+          let s = {};
+          s['payer'] = current.payer;
+          s['points'] = -points;
+          spent.push(s);
+          points = 0;
+        }
+      } else {
+        balances = {};
+        return res
+          .status(400)
+          .json({ msg: 'Cannot enter a new payer with a negative balance' });
+      }
+    } else {
+      let index = spent.findIndex((obj) => obj.payer === current.payer);
+      if (current.points < 0) {
+        let s = spent[index];
+        if (s['points'] - current.points > 0) {
+          balances = {};
+          return res
+            .status(400)
+            .json({ msg: 'Cannot allow a payer to have negative points' });
+        } else {
+          s['points'] -= current.points;
+          balances[current.payer] += current.points;
+          points -= current.points;
+        }
+      } else {
+        balances[current.payer] = current.points;
+
+        if (points > current.points) {
+          points -= current.points;
+          let s = spent[index];
+          s['points'] -= current.points;
+          balances[current.payer] = 0;
+        } else {
+          balances[current.payer] -= points;
+          let s = spent[index];
+          s['points'] -= points;
+          points = 0;
+        }
+      }
+    }
+  }
+
+  res.json(spent);
+});
 
 module.exports = router;
